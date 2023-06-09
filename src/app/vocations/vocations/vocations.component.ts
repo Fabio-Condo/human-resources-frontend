a3808355599ac951a4aa6dc21fafbb2a24adba49
Fábio Condo <fabioenosse59@gmail.com>
2023-06-09 16:43:26 +0200

    update

--- src/app/vocations/vocations/vocations.component.ts
+++ src/app/vocations/vocations/vocations.component.ts
@@ -7,6 +7,8 @@ import { IVocation } from 'src/app/core/interfaces/IVocation';
 import { IVocationFilter } from 'src/app/core/interfaces/IVocationFilter';
 import { HttpErrorResponse } from '@angular/common/http';
 import { IApiResponse } from 'src/app/core/interfaces/IApiResponse';
+import { Vocation } from 'src/app/core/model/Vocation';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-vocations',
@@ -20,6 +22,20 @@ export class VocationsComponent implements OnInit {
   totalRecords: number = 0
 
   vocations: IVocation[] = [];
+
+  employees: any[] = [] ;
+
+  vocation: IVocation = new Vocation;
+  displayModalSave: boolean = false;
+
+  selectedVocationModal: Vocation = new Vocation();
+  displayModal = false;
+
+  vocationStatuses = [
+    { label: 'Aprovado', value: 'APPROVED' },
+    { label: 'Pendente', value: 'PENDING' },
+    { label: 'Rejeitado', value: 'REJECTED' },
+  ];
 
   sizePage = [
     { label: '5 itens por página', value: 5 },
@@ -43,6 +59,8 @@ export class VocationsComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    this.title.setTitle('Vocations page');
+    this.getEmployees();
   }
 
   filter: IVocationFilter = {
@@ -52,6 +70,51 @@ export class VocationsComponent implements OnInit {
   }
 
   @ViewChild('table') grid: any;
+
+  get editing() {
+    return Boolean(this.vocation.id);
+  }
+
+  save(vocationForm: NgForm) {
+    if (this.editing) {
+      this.update(vocationForm)
+    } else {
+      this.addNew(vocationForm)
+    }
+  }
+
+  addNew(vocationForm: NgForm) {
+    this.showLoading = true;
+    this.vocationsService.add(this.vocation).subscribe(
+      (vocationAdded) => {
+        this.vocation = vocationAdded;
+        this.showLoading = false;
+        this.getVocations();
+        this.convertStringsToDates([vocationAdded]);
+        this.messageService.add({ severity: 'success', detail: 'Vocation added successfully' });      
+      },
+      (errorResponse: HttpErrorResponse) => {
+        this.sendErrorNotification(errorResponse.error.message);
+        this.showLoading = false;
+      }
+    );
+  }
+
+  update(vocationForm: NgForm) {
+    this.showLoading = true;
+    this.vocationsService.update(this.vocation).subscribe(
+      (vocation) => {
+        this.vocation = vocation;
+        this.showLoading = false;
+        this.convertStringsToDates([vocation]);
+        this.messageService.add({ severity: 'success', detail: 'Vocation updated successfully!' });
+      },
+      (errorResponse: HttpErrorResponse) => {
+        this.sendErrorNotification(errorResponse.error.message);
+        this.showLoading = false;
+      }
+    )
+  }
 
   getVocations(page: number = 0): void {
     this.showLoading = true;
@@ -69,9 +132,76 @@ export class VocationsComponent implements OnInit {
     );   
   }
 
+  getEmployees() {
+    return this.employeesService.findAll().subscribe(
+      data => {
+        this.employees = data.content.map(employee => {
+          return  {
+            label: employee.name,
+            value: employee.id
+          }
+        })
+      },
+      (errorResponse: HttpErrorResponse) => {
+        this.sendErrorNotification(errorResponse.error.message);
+        this.showLoading = false;
+      }
+    )
+  }
+
+  deleteVocation(vocation: IVocation) {
+    this.vocationsService.delete(vocation.id).subscribe(
+      () => {
+        if (this.grid.first === 0) {
+          this.getVocations();
+        } else {
+          this.grid.reset();
+        }
+        this.messageService.add({ severity: 'success', detail: 'Vocation deleted succefully!' })
+      },
+      (errorResponse: HttpErrorResponse) => {
+        this.sendErrorNotification(errorResponse.error.message);
+        this.showLoading = false;
+      }
+    )
+  }
+
+  deletionConfirm(vocation: IVocation): void {
+    this.confirmationService.confirm({
+      message: 'Are you sure you want to delete?',
+      accept: () => {
+          this.deleteVocation(vocation);
+      }
+    });
+  }
+
+  onAddNewVocation(): void {
+    this.vocation = new Vocation();
+    this.displayModalSave = true;
+  }
+
+  onEditVocation(editVocation: Vocation): void {
+    this.vocation = editVocation;
+    this.vocation.id = editVocation.id // Not necessary
+    this.convertStringsToDates([editVocation]);
+    this.displayModalSave = true;
+  }
+
+  onSelectDepartment(selectedVocation: IVocation): void {
+    this.selectedVocationModal = selectedVocation;
+    this.displayModal = true;
+  }
+
   onChangePage(event: LazyLoadEvent) {
     const page = event!.first! / event!.rows!;  
     this.getVocations(page);
+  }
+
+  private convertStringsToDates(vocations: any[]) {
+    for (const vocation of vocations) {
+      vocation.beginDate = new Date(vocation.beginDate);
+      vocation.endDate = new Date(vocation.endDate);
+    }
   }
 
   private sendErrorNotification(message: string): void {
