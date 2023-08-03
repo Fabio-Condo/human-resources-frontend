b3ab76970d9c56e602721f6345536bcc5c9da673
Fábio Condo <fabioenosse59@gmail.com>
2023-08-03 16:35:03 +0200

    update

--- src/app/employees/employees/employees.component.ts
+++ src/app/employees/employees/employees.component.ts
@@ -184,8 +184,6 @@ export class EmployeesComponent implements OnInit {
 
   _selectedColumns = this.cols;
 
-  products: any[] = [];
-
   constructor(
     private employeesService: EmployeesService,
     private locationsService: LocationsService,
@@ -204,9 +202,6 @@ export class EmployeesComponent implements OnInit {
     this._selectedColumns = [
       //{ field: 'gender', header: 'Gender' }
     ];
-
-    this.products = Array.from({ length: 5 }).map((_, i) => `Item #${i}`);
-    
   }
 
   @Input() get selectedColumns(): any[] {
