73f3ce69a509fca1e5cc2310a26a50667fe0a2be
Fábio Condo <fabioenosse59@gmail.com>
2024-01-03 14:32:48 +0200

    update docker configurations

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://human-resources-production.up.railway.app'
+  apiUrl: 'http://192.168.11.44:85'               // DOCKER // O IP é da minha máquina física dentro da rede. O backend aceita fazer request nesse indereco atraves do postman
+  
+  // O Docker por padrao tem um bridge network que um tem swich que esta conectado a minha rede local, porque isso que usei o IP da minha rede local para fazer a comunicacao entre o backend e o frontend
 };
